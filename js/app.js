e234eb89750335646215c2016f0a63c61f5f1688
nickpwells <wells.nick53@gmail.com>
2015-05-28 19:38:35 -0700

    started js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	var newGame = function(){
+  		var randomNumber = Math.floor((Math.random() * 100) + 1);
+      var userGuess = $("#userGuess").val();
+
+      
+  	}
 
 });
 
