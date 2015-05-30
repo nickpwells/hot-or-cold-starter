94f8307f40d8d3716520855760749f6d1e244bab
nickpwells <wells.nick53@gmail.com>
2015-05-30 13:53:05 -0700

    worked on newgame function

--- js/app.js
+++ js/app.js
@@ -10,12 +10,57 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	var newGame = function(){
-  		var randomNumber = Math.floor((Math.random() * 100) + 1);
-      var userGuess = $("#userGuess").val();
 
+
+    //start a new game
+  	var newGame = function(){
+
+      //computer generates random number
+      function generateRandomNumber() {
+        var randomNumber = Math.floor((Math.random() * 100) + 1);
+        return randomNumber;
+      } 
+      
+      var computerInt = generateRandomNumber();
+
+      this.getUserNumber = function() {
+        return +($('#guessList li:last-child').text());
+      }
+
+      //compare user and computer integer, return true or false
+      function compareInts () {
+        if (userInt === computerInt) {
+          console.log("true");
+          return true;
+        }
+        else {
+          console.log('false');
+          return false;
+        }
+      }
       
   	}
+
+    //display warmer or colder
+    var hotOrCold = function() {
+      var difference = Math.abs(randomNumber - userGuess);
+
+      if (difference >= 50) {window.alert("You're ice cold.");}
+      if (40 <= difference < 50) {window.alert("You're cold, but not freezing.");}
+      if (30 <= difference < 40) {window.alert("You're thawed out.");}
+      if (20 <= difference < 30) {window.alert("You're around 70 degrees right now.  Better give it another shot.");}
+      if (10 <= difference < 20) {window.alert("You better grab some water, it's gettin' hot.");}
+      if (difference < 10) {window.alert("You're hot to the touch!  So close!");}
+    }
+
+    $('#guessButton').click(function(e){
+        e.preventDefault();
+        var userGuess = $('#userGuess').val();
+        $('#guessList').append('<li>'+userGuess+'</li>');
+        newGame.getUserNumber();
+    });
+
+    newGame();
 
 });
 
